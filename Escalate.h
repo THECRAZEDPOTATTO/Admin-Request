92a134a53438db2b2bb6d77e1396ec9024a67674
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-10-10 20:27:11 -0400

    Create Escalate.h

---
+++ Escalate.h
@@ -0,0 +1,10 @@
+#include <string>
+#include <cstdio>
+#include <Windows.h>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+std::string escalate(string file)
+{
+    ShellExecuteA(NULL, "runas", file.c_str(), 0, 0, SW_SHOWNORMAL);
+}
